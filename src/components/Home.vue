091176fc2c9b1597707c7cb61c19eaf2bb49e631
JJ <jj.spetseris@yahoo.com>
2017-09-11 10:11:48 -0500

    progress

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="jumbotron">
+    <h1>Time Tracker</h1>
+    <p>
+      <strong>
+        Get started by <router-link tag="a" to="/time-entries">creating a time entry</router-link>.
+      </strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
